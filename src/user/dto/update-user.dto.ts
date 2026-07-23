b4caa7a476739b5cb370e6f733270e36af2b0f82
uwinezagloria <gloria7wineza@gmail.com>
2026-07-23 17:29:16 +0200

    crud operation ,decoratorator ,search query and parameter route

---
+++ src/user/dto/update-user.dto.ts
@@ -0,0 +1,3 @@
+import { CreateUserDTO } from './create-user.dto';
+import { PartialType } from '@nestjs/mapped-types';
+export class UpdateUserDTO extends PartialType(CreateUserDTO) {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Param, Post, Put, Body } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
-    @Get()
-    getUsers(){
-        return [
-{id:1, name:"Gloria"},{id:2, name:"Uwineza"}
-        ]
+  private users = [
+    { id: 1, name: 'Gloria' },
+    { id: 2, name: 'Uwineza' },
+  ];
+  @Get()
+  // query parameter
+  getUsers(@Query('name') name: string) {
+    if (name) {
+      return this.users.filter(
+        (user) => user.name.toLowerCase() === name.toLowerCase(),
+      );
     }
+    return this.users;
+  }
+  // get user by id
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return this.users.find((user) => user.id === Number(id));
+  }
+  @Post()
+  createUser(@Body() createUserDTO: CreateUserDTO) {
+    const lastUser = this.users[this.users.length - 1];
+    const newId = lastUser.id + 1;
+    const newUser = {
+      id: newId,
+      name: createUserDTO.name,
+    };
+    this.users.push(newUser);
+    return { data: newUser, message: 'user created successfully' };
+  }
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() updateUserDTO: UpdateUserDTO) {
+    const user = this.users.find((user) => user.id === Number(id));
+    if (user) {
+      user.name = updateUserDTO.name;
+    }
+    return { data: updateUserDTO };
+  }
 }
